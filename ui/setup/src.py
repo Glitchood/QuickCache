204794d31c91aceb6c43b44deb12556b28cf925c
Kriv Jhajhria <105179164+Glitchood@users.noreply.github.com>
2024-09-02 13:29:21 -0700

    🎨 Moved setup ui modals/views to separate folder

---
+++ ui/setup/src.py
@@ -0,0 +1,435 @@
+import discord
+from discord.ui import Modal, TextInput, View, Button
+
+
+class ManageChannelsView(View):
+    def __init__(self, *, bot, user_id, category, **kwargs):
+        super().__init__(timeout=300, **kwargs)  # 5 minutes timeout
+        self.bot = bot
+        self.user_id = user_id
+        self.category = category
+        self.tags = []  # Initialize an empty list of tags
+
+    async def interaction_check(self, interaction: discord.Interaction) -> bool:
+        if interaction.user.id != self.user_id:
+            await interaction.response.send_message(
+                "✖️ You are not authorized to use this button.", ephemeral=True
+            )
+            return False
+        return True
+
+    async def on_timeout(self):
+        # This method will be called when the view times out (after 5 minutes)
+        for channel in self.category.channels:
+            await channel.delete()
+        await self.category.delete()
+
+    @discord.ui.button(emoji="➕", label="Add", style=discord.ButtonStyle.success)
+    async def add_channel_button(
+        self, interaction: discord.Interaction, button: Button
+    ):
+        await interaction.response.send_modal(
+            AddChannelModal(bot=self.bot, category=self.category)
+        )
+
+    @discord.ui.button(emoji="➖", label="Delete", style=discord.ButtonStyle.danger)
+    async def remove_channel_button(
+        self, interaction: discord.Interaction, button: Button
+    ):
+        await interaction.response.send_modal(
+            RemoveChannelModal(bot=self.bot, category=self.category)
+        )
+
+    @discord.ui.button(emoji="✏️", label="Rename", style=discord.ButtonStyle.gray)
+    async def rename_channel_button(
+        self, interaction: discord.Interaction, button: Button
+    ):
+        await interaction.response.send_modal(
+            RenameChannelModal(bot=self.bot, category=self.category)
+        )
+
+    @discord.ui.button(emoji="☑️", label="Confirm", style=discord.ButtonStyle.primary)
+    async def confirm_channel_button(
+        self, interaction: discord.Interaction, button: Button
+    ):
+        view = ManageTagsView(bot=self.bot, user_id=self.user_id, tags=self.tags)
+        await interaction.response.send_message(
+            f"Manage **tags** in `{self.category.name}`:", view=view, ephemeral=True
+        )
+
+
+class ManageTagsView(View):
+    def __init__(self, *, bot, user_id, tags, **kwargs):
+        super().__init__(timeout=300, **kwargs)  # 5 minutes timeout
+        self.bot = bot
+        self.user_id = user_id
+        self.tags = tags
+
+    async def interaction_check(self, interaction: discord.Interaction) -> bool:
+        if interaction.user.id != self.user_id:
+            await interaction.response.send_message(
+                "You are not authorized to use this button.", ephemeral=True
+            )
+            return False
+        return True
+
+    async def on_timeout(self):
+        # This method will be called when the view times out (after 5 minutes)
+        self.tags.clear()
+
+    @discord.ui.button(emoji="➕", label="Add", style=discord.ButtonStyle.success)
+    async def add_tag_button(self, interaction: discord.Interaction, button: Button):
+        await interaction.response.send_modal(
+            AddTagModal(bot=self.bot, user_id=self.user_id, tags=self.tags)
+        )
+
+    @discord.ui.button(emoji="➖", label="Remove", style=discord.ButtonStyle.danger)
+    async def remove_tag_button(self, interaction: discord.Interaction, button: Button):
+        await interaction.response.send_modal(
+            RemoveTagModal(bot=self.bot, user_id=self.user_id, tags=self.tags)
+        )
+
+    @discord.ui.button(emoji="✏️", label="Rename", style=discord.ButtonStyle.gray)
+    async def rename_tag_button(self, interaction: discord.Interaction, button: Button):
+        await interaction.response.send_modal(
+            RenameTagModal(bot=self.bot, user_id=self.user_id, tags=self.tags)
+        )
+
+    @discord.ui.button(emoji="☑️", label="Finish", style=discord.ButtonStyle.primary)
+    async def finish_tag_button(self, interaction: discord.Interaction, button: Button):
+        await interaction.response.send_message(
+            f"Tag management completed. \n {self.tags}", ephemeral=True
+        )
+
+
+class ManageCacheView(View):
+    def __init__(self, *, bot, user_id, message_id=None, **kwargs):
+        super().__init__(**kwargs)
+        self.bot = bot
+        self.user_id = user_id
+        self.message_id = message_id  # Store the message_id to delete it later
+
+    @discord.ui.button(label="Setup QuickCache", style=discord.ButtonStyle.primary)
+    async def setup_button(self, interaction: discord.Interaction, button: Button):
+        # Check if the user who clicked the button is the one who invoked the command
+        if interaction.user.id != self.user_id:
+            await interaction.response.send_message(
+                "✖️ You are not authorized to use this button.", ephemeral=True
+            )
+            return
+
+        # Respond to the interaction with the modal
+        await interaction.response.send_modal(
+            ManageCacheModal(bot=self.bot, user_id=self.user_id)
+        )
+
+        # Attempt to delete the original message after responding
+        if self.message_id:
+            try:
+                original_message = await interaction.channel.fetch_message(
+                    self.message_id
+                )
+                await original_message.delete()
+            except discord.NotFound:
+                pass  # Message already deleted or not found
+
+
+class AddTagModal(Modal):
+    def __init__(self, *, bot, user_id, tags, **kwargs):
+        super().__init__(title="Add Tag", **kwargs)
+        self.bot = bot
+        self.user_id = user_id
+        self.tags = tags
+
+        self.tag_input = TextInput(
+            label="Tag",
+            placeholder="Enter a tag...",
+            required=True,
+            min_length=1,
+            max_length=40,
+        )
+        self.add_item(self.tag_input)
+
+    async def on_submit(self, interaction: discord.Interaction):
+        tag = self.tag_input.value
+
+        if tag not in self.tags:
+            self.tags.append(tag)
+            await interaction.response.send_message(
+                f"Tag `{tag}` added.", ephemeral=True
+            )
+        else:
+            await interaction.response.send_message(
+                f"Tag `{tag}` already exists.", ephemeral=True
+            )
+
+
+class RemoveTagModal(Modal):
+    def __init__(self, *, bot, user_id, tags, **kwargs):
+        super().__init__(title="Remove Tag", **kwargs)
+        self.bot = bot
+        self.user_id = user_id
+        self.tags = tags
+
+        self.tag_input = TextInput(
+            label="Tag",
+            placeholder="Enter the tag to remove...",
+            required=True,
+            min_length=1,
+            max_length=40,
+        )
+        self.add_item(self.tag_input)
+
+    async def on_submit(self, interaction: discord.Interaction):
+        tag = self.tag_input.value
+
+        if tag in self.tags:
+            self.tags.remove(tag)
+            await interaction.response.send_message(
+                f"Tag `{tag}` removed.", ephemeral=True
+            )
+        else:
+            await interaction.response.send_message(
+                f"No such tag `{tag}` found.", ephemeral=True
+            )
+
+
+class RenameTagModal(Modal):
+    def __init__(self, *, bot, user_id, tags, **kwargs):
+        super().__init__(title="Rename Tag", **kwargs)
+        self.bot = bot
+        self.user_id = user_id
+        self.tags = tags
+
+        self.current_tag_input = TextInput(
+            label="Current Tag",
+            placeholder="Enter the current tag name...",
+            required=True,
+            min_length=1,
+            max_length=40,
+        )
+        self.new_tag_input = TextInput(
+            label="New Tag",
+            placeholder="Enter the new tag name...",
+            required=True,
+            min_length=1,
+            max_length=40,
+        )
+        self.add_item(self.current_tag_input)
+        self.add_item(self.new_tag_input)
+
+    async def on_submit(self, interaction: discord.Interaction):
+        current_tag = self.current_tag_name.value
+        new_tag = self.new_tag_name.value
+
+        if current_tag not in self.tags:
+            await interaction.response.send_message(
+                f"No tag named `{current_tag}` found.", ephemeral=True
+            )
+            return
+
+        if new_tag in self.tags:
+            await interaction.response.send_message(
+                "A tag with this name already exists. Please choose a different name.",
+                ephemeral=True,
+            )
+            return
+
+        self.tags[self.tags.index(current_tag)] = new_tag
+        await interaction.response.send_message(
+            f"Tag `{current_tag}` renamed to `{new_tag}`.", ephemeral=True
+        )
+
+
+class RemoveChannelModal(Modal):
+    def __init__(self, *, bot, category, **kwargs):
+        super().__init__(title="Remove a Category from QuickCache", **kwargs)
+        self.bot = bot
+        self.category = category
+
+        self.channel_name = TextInput(
+            label="Category Name",
+            placeholder="Enter the name of the CHANNEL to remove...",
+            required=True,
+            min_length=1,
+            max_length=40,
+        )
+        self.add_item(self.channel_name)
+
+    async def on_submit(self, interaction: discord.Interaction):
+        channel_name = self.channel_name.value
+        channel = discord.utils.get(self.category.channels, name=channel_name)
+
+        if channel:
+            await channel.delete()
+            await interaction.response.send_message(
+                f"✔️ Category `{channel_name}` removed from `{self.category.name}`.",
+                ephemeral=True,
+            )
+        else:
+            await interaction.response.send_message(
+                f"✖️ No channel named `{channel_name}` found in `{self.category.name}`.",
+                ephemeral=True,
+            )
+
+    async def on_error(
+        self, interaction: discord.Interaction, error: Exception
+    ) -> None:
+        await interaction.response.send_message(
+            "✖️ Oops! Something went wrong.", ephemeral=True
+        )
+        import traceback
+
+        traceback.print_exception(type(error), error, error.__traceback__)
+
+
+class RenameChannelModal(Modal):
+    def __init__(self, *, bot, category, **kwargs):
+        super().__init__(title="Rename a Category in QuickCache", **kwargs)
+        self.bot = bot
+        self.category = category
+
+        self.current_channel_name = TextInput(
+            label="Current Category Name",
+            placeholder="Enter the current name of the CHANNEL...",
+            required=True,
+            min_length=1,
+            max_length=40,
+        )
+        self.add_item(self.current_channel_name)
+
+        self.new_channel_name = TextInput(
+            label="New Category Name",
+            placeholder="Enter the new name for the category...",
+            required=True,
+            min_length=1,
+            max_length=40,
+        )
+        self.add_item(self.new_channel_name)
+
+    async def on_submit(self, interaction: discord.Interaction):
+        current_channel_name = self.current_channel_name.value
+        new_channel_name = self.new_channel_name.value
+
+        channel = discord.utils.get(self.category.channels, name=current_channel_name)
+
+        new_channel = discord.utils.get(self.category.channels, name=new_channel_name)
+
+        if channel:
+            if not new_channel:
+                await channel.edit(name=new_channel_name)
+                await interaction.response.send_message(
+                    f"✔️ Category `{current_channel_name}` renamed to `{new_channel_name}`.",  # noqa E501
+                    ephemeral=True,
+                )
+            else:
+                await interaction.response.send_message(
+                    f"✖️ Category `{new_channel_name}` already exists.",
+                    ephemeral=True,
+                )
+        else:
+            await interaction.response.send_message(
+                f"✖️ No channel named `{current_channel_name}` found in `{self.category.name}`.",  # noqa E501
+                ephemeral=True,
+            )
+
+    async def on_error(
+        self, interaction: discord.Interaction, error: Exception
+    ) -> None:
+        await interaction.response.send_message(
+            "✖️ Oops! Something went wrong.", ephemeral=True
+        )
+        import traceback
+
+        traceback.print_exception(type(error), error, error.__traceback__)
+
+
+class AddChannelModal(Modal):
+    def __init__(self, *, bot, category, **kwargs):
+        super().__init__(title="Add a Category to QuickCache", **kwargs)
+        self.bot = bot
+        self.category = category
+
+        self.channel_name = TextInput(
+            label="Category Name",
+            placeholder="Enter the name for the new category...",
+            required=True,
+            min_length=1,
+            max_length=40,
+        )
+        self.add_item(self.channel_name)
+
+    async def on_submit(self, interaction: discord.Interaction):
+        channel_name = self.channel_name.value
+
+        channel = discord.utils.get(self.category.channels, name=channel_name)
+
+        if channel:
+            await interaction.response.send_message(
+                f"✖️ Category `{channel_name}` already exists.",
+                ephemeral=True,
+            )
+        else:
+            await self.category.create_text_channel(name=channel_name)
+            await interaction.response.send_message(
+                f"✔️ Category `{channel_name}` added to `{self.category.name}`.",
+                ephemeral=True,
+            )
+
+    async def on_error(
+        self, interaction: discord.Interaction, error: Exception
+    ) -> None:
+        await interaction.response.send_message(
+            "✖️ Oops! Something went wrong.", ephemeral=True
+        )
+        import traceback
+
+        traceback.print_exception(type(error), error, error.__traceback__)
+
+
+class ManageCacheModal(Modal):
+    def __init__(self, *, bot, user_id, **kwargs):
+        super().__init__(title="Setup a new QuickCache", **kwargs)
+        self.bot = bot
+        self.user_id = user_id
+
+        # Add a text input to the modal
+        self.cache_name = TextInput(
+            label="Cache Name",
+            placeholder="Enter the name for the cache...",
+            required=True,
+            min_length=1,
+            max_length=40,
+        )
+        self.add_item(self.cache_name)
+
+    async def on_submit(self, interaction: discord.Interaction):
+        cache_name = self.cache_name.value
+        category_name = f"[QC] {cache_name}"
+
+        if not interaction.guild.me.guild_permissions.manage_channels:
+            await interaction.response.send_message(
+                "✖️ I don't have permission to manage channels.", ephemeral=True
+            )
+            return
+
+        category = await interaction.guild.create_category(category_name)
+        await interaction.response.send_message(
+            f"✔️ Cache `{cache_name}` setup successfully!", ephemeral=True
+        )
+
+        # Send a follow-up message with options to add or remove channels
+        view = ManageChannelsView(bot=self.bot, user_id=self.user_id, category=category)
+        await interaction.followup.send(
+            f"Manage **categories** in `{cache_name}`:", view=view, ephemeral=True
+        )
+
+    async def on_error(
+        self, interaction: discord.Interaction, error: Exception
+    ) -> None:
+        await interaction.response.send_message(
+            "✖️ Oops! Something went wrong.", ephemeral=True
+        )
+        import traceback
+
+        traceback.print_exception(type(error), error, error.__traceback__)
